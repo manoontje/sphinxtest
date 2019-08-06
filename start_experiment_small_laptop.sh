38d67f57628df78c4227a3d8dec5f937c81f7b2c
Tjalling Haije <tjalling_haije@outlook.com>
2019-08-06 10:42:28 +0200

    startup script small laptop

---
+++ start_experiment_small_laptop.sh
@@ -0,0 +1,8 @@
+google-chrome file:///home/hal/Projects/tasking-constraint-learning/experiment/tutorial/intro.html &
+cd ~/Projects/matrxs/visualization
+python3.6 server.py &
+google-chrome http://localhost:3000/god &
+cd ~/Projects/matrxs/
+export FLASK_APP=run_experiment.py
+flask run -p 3001 &
+google-chrome http://localhost:3001 &
